388cb439092f299380cc645247a90e1966229db3
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-11-22 17:53:19 +0300

    Added TypeInfo

--- AssemblyBrowser/AssemblyBrowserLib/TypeInfo.cs
+++ AssemblyBrowser/AssemblyBrowserLib/TypeInfo.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using System.Reflection;
 
 namespace AssemblyBrowserLib
 {
     class TypeInfo
     {
         public string Name { get; }
-        private ObservableCollection<TypeMemberInfo> _members = new ObservableCollection<TypeMemberInfo>();
-        public ReadOnlyObservableCollection<TypeMemberInfo> Members;
+        private Type _type;
+        private List<TypeMemberInfo> _members;
+        public IReadOnlyList<TypeMemberInfo> Info { get { return GetInfo(); } }
 
         public TypeInfo(Type type)
         {
-            Members = new ReadOnlyObservableCollection<TypeMemberInfo>(_members);
+            _type = type;
+            _members = new List<TypeMemberInfo>();
+        }
+
+        private IReadOnlyList<TypeMemberInfo> GetInfo()
+        {
+            
+            var result = from mem in _type.GetMembers()
+                         where mem.MemberType == MemberTypes.Method || 
+                               mem.MemberType == MemberTypes.Field ||
+                               mem.MemberType == MemberTypes.Property
+                         select new TypeMemberInfo(mem);
+            return new List<TypeMemberInfo>(result);
         }
     }
 }
