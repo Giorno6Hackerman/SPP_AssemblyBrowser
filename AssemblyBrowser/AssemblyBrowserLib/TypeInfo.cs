8f9f1f8b30e0166f78de9b064e4baa248f6df7cd
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-11-22 17:24:22 +0300

    Added TypeMemberInfo

--- AssemblyBrowser/AssemblyBrowserLib/TypeInfo.cs
+++ AssemblyBrowser/AssemblyBrowserLib/TypeInfo.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace AssemblyBrowserLib
 {
     class TypeInfo
     {
+        public string Name { get; }
+        private ObservableCollection<TypeMemberInfo> _members = new ObservableCollection<TypeMemberInfo>();
+        public ReadOnlyObservableCollection<TypeMemberInfo> Members;
+
+        public TypeInfo(Type type)
+        {
+            Members = new ReadOnlyObservableCollection<TypeMemberInfo>(_members);
+        }
     }
 }
---
+++ AssemblyBrowser/AssemblyBrowserLib/TypeMemberInfo.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+
+namespace AssemblyBrowserLib
+{
+    class TypeMemberInfo
+    {
+        private MemberInfo _memberInfo { get; }
+
+        public TypeMemberInfo(MemberInfo memberInfo)
+        {
+            _memberInfo = memberInfo;
+        }
+
+        public string Info { get { return GetInfo(_memberInfo); } }
+
+        private string GetMemberInfo(MethodInfo info)
+        {
+            string result = $"Method {info.Name} (";
+            foreach (var param in info.GetParameters())
+            {
+                result += $"{param.ParameterType} {param.Name}, ";
+            }
+            result.Remove(result.Length - 2);
+            return result + ")";
+        }
+
+        private string GetMemberInfo(FieldInfo info)
+        {
+            return $"Field {info.FieldType} {info.Name}";
+        }
+
+        private string GetMemberInfo(PropertyInfo info)
+        {
+            return $"Property {info.PropertyType} {info.Name}";
+        }
+
+
+        private string GetInfo(MemberInfo info)
+        {
+            if (info.MemberType == MemberTypes.Method)
+            {
+                return GetMemberInfo(info as MethodInfo);
+            }
+            if (info.MemberType == MemberTypes.Property)
+            {
+                return GetMemberInfo(info as PropertyInfo);
+            }
+            if (info.MemberType == MemberTypes.Field)
+            {
+                return GetMemberInfo(info as FieldInfo);
+            }
+            return null;
+        }
+    }
+}
