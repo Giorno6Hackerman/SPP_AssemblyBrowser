b193638e8c885279b5df2af415cc6eb4c31356ac
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-11-22 19:46:28 +0300

    Added AssemblyInfo

--- AssemblyBrowser/AssemblyBrowserLib/AssemblyInfo.cs
+++ AssemblyBrowser/AssemblyBrowserLib/AssemblyInfo.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Reflection;
+using System.Linq;
 
 namespace AssemblyBrowserLib
 {
     class AssemblyInfo
     {
+        public string Name { get; }
+        private string _path;
+        private Assembly _asm;
+        public IReadOnlyList<NamespaceInfo> Namespaces { get { return GetInfo(); } }
+
+        public AssemblyInfo(string path)
+        {
+            _path = path;
+            Name = AssemblyName.GetAssemblyName(path).ToString();
+        }
+
+        private IReadOnlyList<NamespaceInfo> GetInfo()
+        {
+            _asm = Assembly.LoadFrom(_path);
+            var namespaces = from type in _asm.GetTypes()
+                             group type by type.Namespace;
+            var result = from namespc in namespaces
+                         select new NamespaceInfo(namespc.Key, new List<Type>(namespc.Where(type => type.Namespace == namespc.Key)));
+            return new List<NamespaceInfo>(result);
+        }
     }
 }
